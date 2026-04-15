299bef22ede7efab94688f08a0c0e39c7723a259
CrNazamOfficial <creatornazamofficial@gmail.com>
2026-04-15 18:13:03 +0700

    MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,47 @@
+package com.zam.apkbuilder;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        String sourceType = "__SOURCE_TYPE__";
+        if (sourceType.equals("url")) {
+            webView.loadUrl("__WEBVIEW_URL__");
+        } else {
+            String html = "__HTML_CONTENT__";
+            webView.loadDataWithBaseURL(null, html, "text/html", "UTF-8", null);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+  }
